40a8692ee449c81109762e6831b72991522e4db5
pablo-rodr-bio2 <pablo.rodriguez@toptal.com>
2024-12-21 12:50:27 +0100

    Refactored updateUnit logic outside setter

--- src/hooks/useUnits.tsx
+++ src/hooks/useUnits.tsx
@@ -2,6 +2,22 @@ import { ProcessedTicket, StoredTicket } from "@/types/ticket";
 import { useEffect, useState } from "react";
 import { getUnitStoreFromSessionStorage, setUnitStoreToSessionStorage } from "../utils/session-storage";
 
+function updateUnitStore(prevUnitStore: StoredTicket[], ticket: ProcessedTicket, newUnit: number) {
+  if (newUnit === 0) {
+    return prevUnitStore.filter((item) => item.ticket.id !== ticket.id);
+  }
+
+  const existingIndex = prevUnitStore.findIndex((item) => item.ticket.id  === ticket.id);
+
+  if (existingIndex === -1) {
+    return [...prevUnitStore, { ticket, unit: newUnit }];
+  }
+
+  return prevUnitStore.map((item, index) => 
+    index === existingIndex ? { ...item, unit: newUnit } : item
+  );      
+}
+
 function useUnits() {
   const [unitStore, setUnitStore] = useState<StoredTicket[]>(
     getUnitStoreFromSessionStorage()
@@ -12,21 +28,7 @@ function useUnits() {
   }, [unitStore]);
 
   const updateUnit = (ticket: ProcessedTicket, newUnit: number) => {
-    setUnitStore(prevUnitStore => {
-      if (newUnit === 0) {
-        return prevUnitStore.filter((item) => item.ticket.id !== ticket.id);
-      }
-
-      const existingIndex = prevUnitStore.findIndex((item) => item.ticket.id  === ticket.id);
-
-      if (existingIndex === -1) {
-        return [...prevUnitStore, { ticket, unit: newUnit }];
-      }
-
-      return prevUnitStore.map((item, index) => 
-        index === existingIndex ? { ...item, unit: newUnit } : item
-      );      
-    })
+    setUnitStore(prevUnitStore => updateUnitStore(prevUnitStore, ticket, newUnit));
   }
 
   return {
